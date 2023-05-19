79aa49627e19dd3de27339c7062bc5d4aa4816bc
Maria Romanchuk <mashulya_r_88@mail.ru>
2023-05-19 18:45:29 +0700

    Initial Commit

---
+++ BioBlitz/BioBlitzApp.swift
@@ -0,0 +1,17 @@
+//
+//  BioBlitzApp.swift
+//  BioBlitz
+//
+//  Created by Мария Романчук on 19.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct BioBlitzApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
